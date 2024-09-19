b1fd7662a37331f5cbc370356ccae06a511f9c3a
Abdullah Tarek <paysky@EG-iOS-AbdullahTarek.local>
2024-09-19 21:54:13 +0300

    first commit

---
+++ RepoFinder/RepoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoFinderApp.swift
+//  RepoFinder
+//
+//  Created by Abdullah Tarek on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
